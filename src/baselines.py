15eef8626cdeeb357ad918ed35779dabee27b38e
Elisa <elisamecp@gmail.com>
2023-05-15 17:18:15 +0200

    Add RNN params

--- src/baselines.py
+++ src/baselines.py
@@ -132,33 +132,33 @@ class RNNMunchausenDeepMirrorDescent:
         num_actions = env.action_spec()["num_actions"]
 
         kwargs = {
-            "alpha": cfg['munchausen_omd']['alpha'],
-            "batch_size": cfg['munchausen_omd']['batch_size'],
-            "discount_factor": cfg['munchausen_omd']['discount_factor'],
-            "epsilon_decay_duration": cfg['munchausen_omd']['epsilon_decay_duration'],
-            "epsilon_end": cfg['munchausen_omd']['epsilon_end'],
-            "epsilon_power": cfg['munchausen_omd']['epsilon_power'],
-            "epsilon_start": cfg['munchausen_omd']['epsilon_start'],
-            "gradient_clipping": cfg['munchausen_omd']['gradient_clipping'],
-            "hidden_layers_sizes": [int(l) for l in cfg['munchausen_omd']['hidden_layers_sizes']],
-            "huber_loss_parameter": cfg['munchausen_omd']['huber_loss_parameter'],
-            "learn_every": cfg['munchausen_omd']['learn_every'],
-            "learning_rate": cfg['munchausen_omd']['learning_rate'],
-            "loss": cfg['munchausen_omd']['loss'],
-            "min_buffer_size_to_learn": cfg['munchausen_omd']['min_buffer_size_to_learn'],
-            "optimizer": cfg['munchausen_omd']['optimizer'],
-            "replay_buffer_capacity": cfg['munchausen_omd']['replay_buffer_capacity'],
-            "reset_replay_buffer_on_update": cfg['munchausen_omd']['reset_replay_buffer_on_update'],
-            "seed": cfg['munchausen_omd']['seed'],
-            "tau": cfg['munchausen_omd']['tau'],
-            "update_target_network_every": cfg['munchausen_omd']['update_target_network_every'],
-            "with_munchausen": cfg['munchausen_omd']['with_munchausen']
+            "alpha": cfg['rnn_munchausen_omd']['alpha'],
+            "batch_size": cfg['rnn_munchausen_omd']['batch_size'],
+            "discount_factor": cfg['rnn_munchausen_omd']['discount_factor'],
+            "epsilon_decay_duration": cfg['rnn_munchausen_omd']['epsilon_decay_duration'],
+            "epsilon_end": cfg['rnn_munchausen_omd']['epsilon_end'],
+            "epsilon_power": cfg['rnn_munchausen_omd']['epsilon_power'],
+            "epsilon_start": cfg['rnn_munchausen_omd']['epsilon_start'],
+            "gradient_clipping": cfg['rnn_munchausen_omd']['gradient_clipping'],
+            "hidden_layers_sizes": [int(l) for l in cfg['rnn_munchausen_omd']['hidden_layers_sizes']],
+            "huber_loss_parameter": cfg['rnn_munchausen_omd']['huber_loss_parameter'],
+            "learn_every": cfg['rnn_munchausen_omd']['learn_every'],
+            "learning_rate": cfg['rnn_munchausen_omd']['learning_rate'],
+            "loss": cfg['rnn_munchausen_omd']['loss'],
+            "min_buffer_size_to_learn": cfg['rnn_munchausen_omd']['min_buffer_size_to_learn'],
+            "optimizer": cfg['rnn_munchausen_omd']['optimizer'],
+            "replay_buffer_capacity": cfg['rnn_munchausen_omd']['replay_buffer_capacity'],
+            "reset_replay_buffer_on_update": cfg['rnn_munchausen_omd']['reset_replay_buffer_on_update'],
+            "seed": cfg['rnn_munchausen_omd']['seed'],
+            "tau": cfg['rnn_munchausen_omd']['tau'],
+            "update_target_network_every": cfg['rnn_munchausen_omd']['update_target_network_every'],
+            "with_munchausen": cfg['rnn_munchausen_omd']['with_munchausen']
         }
         agents = [RNN_munchausen_deep_mirror_descent.RNNMunchausenDQN(p, info_state_size, num_actions, **kwargs)
                 for p in range(num_players)]
 
-        num_episodes_per_iteration = cfg['munchausen_omd']['num_episodes_per_iteration']
-        self.md = RNN_munchausen_deep_mirror_descent.RNNDeepOnlineMirrorDescent(self.game, envs, agents, eval_every=cfg['munchausen_omd']['eval_every'],
+        num_episodes_per_iteration = cfg['rnn_munchausen_omd']['num_episodes_per_iteration']
+        self.md = RNN_munchausen_deep_mirror_descent.RNNDeepOnlineMirrorDescent(self.game, envs, agents, eval_every=cfg['rnn_munchausen_omd']['eval_every'],
                                                                         num_episodes_per_iteration=num_episodes_per_iteration)
 
     def solve(self):
