6bbf4b8ab2e496878e3c9aa089b8aa1bdacd241e
Elisa <elisamecp@gmail.com>
2023-06-14 15:50:08 +0200

    Add logdir option

--- src/baselines.py
+++ src/baselines.py
@@ -37,10 +37,10 @@ class MFGAlgorithm:
 
 
 class MunchausenDeepMirrorDescent(MFGAlgorithm):
-    def __init__(self, game_name, game_setting, cfg):
+    def __init__(self, game_name, game_setting, cfg, logdir='runs/D-MOMD'):
         super().__init__(game_name, game_setting, cfg)
 
-        self.writer = SummaryWriter('runs/D-MOMD')
+        self.writer = SummaryWriter(logdir)
 
         envs = [rl_environment.Environment(self.game, mfg_distribution=self.uniform_dist, mfg_population=p,
                 observation_type=rl_environment.ObservationType.OBSERVATION) for p in range(self.num_players)]
@@ -80,10 +80,9 @@ class MunchausenDeepMirrorDescent(MFGAlgorithm):
                                                                         num_episodes_per_iteration=num_episodes_per_iteration)
 
 class ObsMunchausenDeepMirrorDescent(MFGAlgorithm):
-    def __init__(self, game_name, game_setting, cfg, observability=False):
+    def __init__(self, game_name, game_setting, cfg, observability=False, logdir='runs/FO-DMOMD'):
         super().__init__(game_name, game_setting, cfg)
 
-        logdir = 'runs/PO-DMOMD' if observability else 'runs/FO-DMOMD'
         self.writer = SummaryWriter(logdir)
 
         envs = [rl_obs_environment.ObsEnvironment(self.game, mfg_distribution=self.uniform_dist, mfg_population=p,
@@ -127,10 +126,10 @@ class ObsMunchausenDeepMirrorDescent(MFGAlgorithm):
 
 
 class AverageFictitiousPlay(MFGAlgorithm):
-    def __init__(self, game_name, game_setting, cfg):
+    def __init__(self, game_name, game_setting, cfg, logdir='runs/D-AFP'):
         super().__init__(game_name, game_setting, cfg)
 
-        self.writer = SummaryWriter('runs/D-AFP')
+        self.writer = SummaryWriter(logdir)
 
         self.envs = [rl_environment.Environment(self.game, mfg_distribution=self.uniform_dist, mfg_population=p) for p in range(self.num_players)]
 
@@ -193,10 +192,9 @@ class AverageFictitiousPlay(MFGAlgorithm):
         return expls
 
 class ObsAverageFictitiousPlay(MFGAlgorithm):
-    def __init__(self, game_name, game_setting, cfg, observability=False):
+    def __init__(self, game_name, game_setting, cfg, observability=False, logdir='runs/FO-DAFP'):
         super().__init__(game_name, game_setting, cfg)
 
-        logdir = 'runs/PO-DAFP' if observability else 'runs/FO-DAFP'
         self.writer = SummaryWriter(logdir)
 
         self.envs = [rl_obs_environment.ObsEnvironment(self.game, mfg_distribution=self.uniform_dist, mfg_population=p, partial_obs=observability) for p in range(self.num_players)]
@@ -263,10 +261,53 @@ class ObsAverageFictitiousPlay(MFGAlgorithm):
 
 
 class RNNMunchausenDeepMirrorDescent(MFGAlgorithm):
-    def __init__(self, game_name, game_setting, cfg):
+    def __init__(self, game_name, game_setting, cfg, logdir='runs/RNN-MOMD'):
         super().__init__(game_name, game_setting, cfg)
 
-        self.writer = SummaryWriter('runs/RNN-MOMD')
+        self.writer = SummaryWriter(logdir)
+
+        envs = [rl_environment.Environment(self.game, mfg_distribution=self.uniform_dist, mfg_population=p,
+                observation_type=rl_environment.ObservationType.OBSERVATION) for p in range(self.num_players)]
+
+        env = envs[0]
+        info_state_size = env.observation_spec()["info_state"][0]
+        num_actions = env.action_spec()["num_actions"]
+
+        kwargs = {
+            "alpha": cfg['rnn_munchausen_omd']['alpha'],
+            "batch_size": cfg['rnn_munchausen_omd']['batch_size'],
+            "discount_factor": cfg['rnn_munchausen_omd']['discount_factor'],
+            "epsilon_decay_duration": cfg['rnn_munchausen_omd']['epsilon_decay_duration'],
+            "epsilon_end": cfg['rnn_munchausen_omd']['epsilon_end'],
+            "epsilon_power": cfg['rnn_munchausen_omd']['epsilon_power'],
+            "epsilon_start": cfg['rnn_munchausen_omd']['epsilon_start'],
+            "gradient_clipping": cfg['rnn_munchausen_omd']['gradient_clipping'],
+            "hidden_layers_sizes": [int(l) for l in cfg['rnn_munchausen_omd']['hidden_layers_sizes']],
+            "huber_loss_parameter": cfg['rnn_munchausen_omd']['huber_loss_parameter'],
+            "learn_every": cfg['rnn_munchausen_omd']['learn_every'],
+            "learning_rate": cfg['rnn_munchausen_omd']['learning_rate'],
+            "loss": cfg['rnn_munchausen_omd']['loss'],
+            "min_buffer_size_to_learn": cfg['rnn_munchausen_omd']['min_buffer_size_to_learn'],
+            "optimizer": cfg['rnn_munchausen_omd']['optimizer'],
+            "replay_buffer_capacity": cfg['rnn_munchausen_omd']['replay_buffer_capacity'],
+            "reset_replay_buffer_on_update": cfg['rnn_munchausen_omd']['reset_replay_buffer_on_update'],
+            "seed": cfg['rnn_munchausen_omd']['seed'],
+            "tau": cfg['rnn_munchausen_omd']['tau'],
+            "update_target_network_every": cfg['rnn_munchausen_omd']['update_target_network_every'],
+            "with_munchausen": cfg['rnn_munchausen_omd']['with_munchausen']
+        }
+        agents = [rnn_momd.RNNMunchausenDQN(p, info_state_size, num_actions, **kwargs)
+                for p in range(self.num_players)]
+
+        num_episodes_per_iteration = cfg['rnn_munchausen_omd']['num_episodes_per_iteration']
+        self.md = rnn_momd.RNNDeepOnlineMirrorDescent(self.game, envs, agents, eval_every=cfg['rnn_munchausen_omd']['eval_every'],
+                                                                        num_episodes_per_iteration=num_episodes_per_iteration)
+
+class ObsRNNMunchausenDeepMirrorDescent(MFGAlgorithm):
+    def __init__(self, game_name, game_setting, cfg, observability=False, logdir='runs/FO-RNN-MOMD'):
+        super().__init__(game_name, game_setting, cfg)
+
+        self.writer = SummaryWriter(logdir)
 
         envs = [rl_environment.Environment(self.game, mfg_distribution=self.uniform_dist, mfg_population=p,
                 observation_type=rl_environment.ObservationType.OBSERVATION) for p in range(self.num_players)]
--- src/evaluation.py
+++ src/evaluation.py
@@ -6,6 +6,7 @@ from typing import Sequence
 FLAGS = flags.FLAGS
 flags.DEFINE_string('config', './configs/crowd_config.yml', 'Config file with the algorithms parameters.')
 flags.DEFINE_string('algorithm', './configs/crowd_config.yml', 'Config file with the algorithms parameters.')
+flags.DEFINE_string('logdir', None, 'Directory to save the logs')
 
 def main(argv: Sequence[str]):
     cfg = utils.read_config(FLAGS.config)
@@ -16,23 +17,34 @@ def main(argv: Sequence[str]):
     alg = None
 
     if FLAGS.algorithm == 'dafp':
-        alg = AverageFictitiousPlay(game_name, game_settings, cfg)
+        logdir = 'runs/D-AFP' if not FLAGS.logdir else FLAGS.logdir
+        alg = AverageFictitiousPlay(game_name, game_settings, cfg, logdir=logdir)
     elif FLAGS.algorithm == 'dmomd':
-        alg = MunchausenDeepMirrorDescent(game_name, game_settings, cfg)
+        logdir = 'runs/D-MOMD' if not FLAGS.logdir else FLAGS.logdir
+        alg = MunchausenDeepMirrorDescent(game_name, game_settings, cfg, logdir=logdir)
     elif FLAGS.algorithm == 'rnn-momd':
-        alg = RNNMunchausenDeepMirrorDescent(game_name, game_settings, cfg)
+        logdir = 'runs/RNN-MOMD' if not FLAGS.logdir else FLAGS.logdir
+        alg = RNNMunchausenDeepMirrorDescent(game_name, game_settings, cfg, logdir=logdir)
 
     elif FLAGS.algorithm == 'fo-dafp':
-        alg = ObsAverageFictitiousPlay(game_name, game_settings, cfg)
+        logdir = 'runs/FO-DAFP' if not FLAGS.logdir else FLAGS.logdir
+        alg = ObsAverageFictitiousPlay(game_name, game_settings, cfg, logdir=logdir)
     elif FLAGS.algorithm == 'fo-dmomd':
-        alg = ObsMunchausenDeepMirrorDescent(game_name, game_settings, cfg)
+        logdir = 'runs/FO-DMOMD' if not FLAGS.logdir else FLAGS.logdir
+        alg = ObsMunchausenDeepMirrorDescent(game_name, game_settings, cfg, logdir=logdir)
+    elif FLAGS.algorithm == 'fo-rnn-momd':
+        logdir = 'runs/FO-RNNMOMD' if not FLAGS.logdir else FLAGS.logdir
+        raise ObsRNNMunchausenDeepMirrorDescent(game_name, game_settings, cfg, logdir=logdir)
 
     elif FLAGS.algorithm == 'po-dafp':
-        alg = ObsAverageFictitiousPlay(game_name, game_settings, cfg, observability=True)
+        logdir = 'runs/PO-DAFP' if not FLAGS.logdir else FLAGS.logdir
+        alg = ObsAverageFictitiousPlay(game_name, game_settings, cfg, observability=True, logdir=logdir)
     elif FLAGS.algorithm == 'po-dmomd':
-        alg = ObsMunchausenDeepMirrorDescent(game_name, game_settings, cfg, observability=True)
+        logdir = 'runs/PO-DMOMD' if not FLAGS.logdir else FLAGS.logdir
+        alg = ObsMunchausenDeepMirrorDescent(game_name, game_settings, cfg, observability=True, logdir=logdir)
     elif FLAGS.algorithm == 'po-rnn-momd':
-        raise NotImplementedError
+        logdir = 'runs/PO-RNNMOMD' if not FLAGS.logdir else FLAGS.logdir
+        raise ObsRNNMunchausenDeepMirrorDescent(game_name, game_settings, cfg, observability=True, logdir=logdir)
     else:
         print("usage: evaluation.py --config=<config_dir> --algorithm=['afp', 'dmomd', 'rnn-momd', 'fo-dmomd', 'po-dmomd', 'po-rnn-momd']")
         exit()
